8ecd883934ab2c6c4975966b8beeec210dbf3256
Abolfazl <118627883+Abizzzi@users.noreply.github.com>
2024-09-11 22:05:57 +0330

    Add files via upload

---
+++ Click_game/js/style.js
@@ -0,0 +1,76 @@
+﻿$(window).ready(function(e) 
+{
+	var a = 0;
+	var once = 0;
+	var second = 0;
+	var b = 0;
+    $(".c1").mousedown(function(e){ $(".c1").css("display","none"); a++; Abol(); });
+	$(".c2").mousedown(function(e){ $(".c2").css("display","none"); a++; Abol(); });
+	$(".c3").mousedown(function(e){ $(".c3").css("display","none"); a++; Abol(); });
+	$(".c4").mousedown(function(e){ $(".c4").css("display","none"); a++; Abol(); });
+	$(".c5").mousedown(function(e){ $(".c5").css("display","none"); a++; Abol(); });
+	$(".c6").mousedown(function(e){ $(".c6").css("display","none"); a++; Abol(); });
+	$(".c7").mousedown(function(e){ $(".c7").css("display","none"); a++; Abol(); });
+	$(".c8").mousedown(function(e){ $(".c8").css("display","none"); a++; Abol(); });
+	$(".c9").mousedown(function(e){ $(".c9").css("display","none"); a++; Abol(); });
+	$(".c10").mousedown(function(e){ $(".c10").css("display","none"); a++; Abol(); });
+	$(".c11").mousedown(function(e){ $(".c11").css("display","none"); a++; Abol(); });
+	$(".c12").mousedown(function(e){ $(".c12").css("display","none"); a++; Abol(); });
+	$(".c13").mousedown(function(e){ $(".c13").css("display","none"); a++; Abol(); });
+	$(".c14").mousedown(function(e){ $(".c14").css("display","none"); a++; Abol(); });
+	$(".c15").mousedown(function(e){ $(".c15").css("display","none"); a++; Abol(); });
+	$(".c16").mousedown(function(e){ $(".c16").css("display","none"); a++; Abol(); });
+	$(".c17").mousedown(function(e){ $(".c17").css("display","none"); a++; Abol(); });
+	$(".c18").mousedown(function(e){ $(".c18").css("display","none"); a++; Abol(); });
+	$(".c19").mousedown(function(e){ $(".c19").css("display","none"); a++; Abol(); });
+	$(".c20").mousedown(function(e){ $(".c20").css("display","none"); a++; Abol(); });
+	$(".c21").mousedown(function(e){ $(".c21").css("display","none"); a++; Abol(); });
+	$(".c22").mousedown(function(e){ $(".c22").css("display","none"); a++; Abol(); });
+	$(".c23").mousedown(function(e){ $(".c23").css("display","none"); a++; Abol(); });
+	$(".c24").mousedown(function(e){ $(".c24").css("display","none"); a++; Abol(); });
+	$(".c25").mousedown(function(e){ $(".c25").css("display","none"); a++; Abol(); });
+	$(".c26").mousedown(function(e){ $(".c26").css("display","none"); a++; Abol(); });
+	$(".c27").mousedown(function(e){ $(".c27").css("display","none"); a++; Abol(); });
+	$(".c28").mousedown(function(e){ $(".c28").css("display","none"); a++; Abol(); });
+	$(".c29").mousedown(function(e){ $(".c29").css("display","none"); a++; Abol(); });
+	$(".c30").mousedown(function(e){ $(".c30").css("display","none"); a++; Abol(); });
+	$(".c31").mousedown(function(e){ $(".c31").css("display","none"); a++; Abol(); });
+	$(".c32").mousedown(function(e){ $(".c32").css("display","none"); a++; Abol(); });
+	$(".c33").mousedown(function(e){ $(".c33").css("display","none"); a++; Abol(); });
+	$(".c34").mousedown(function(e){ $(".c34").css("display","none"); a++; Abol(); });
+	$(".c35").mousedown(function(e){ $(".c35").css("display","none"); a++; Abol(); });
+	$(".c36").mousedown(function(e){ $(".c36").css("display","none"); a++; Abol(); });
+	$(".c37").mousedown(function(e){ $(".c37").css("display","none"); a++; Abol(); });
+	$(".c38").mousedown(function(e){ $(".c38").css("display","none"); a++; Abol(); });
+	$(".c39").mousedown(function(e){ $(".c39").css("display","none"); a++; Abol(); });
+	$(".c40").mousedown(function(e){ $(".c40").css("display","none"); a++; Abol(); });
+	$(".c41").mousedown(function(e){ $(".c41").css("display","none"); a++; Abol(); });
+	$(".c42").mousedown(function(e){ $(".c42").css("display","none"); a++; Abol(); });
+	$(".c43").mousedown(function(e){ $(".c43").css("display","none"); a++; Abol(); });
+	$(".c44").mousedown(function(e){ $(".c44").css("display","none"); a++; Abol(); });
+	$(".c45").mousedown(function(e){ $(".c45").css("display","none"); a++; Abol(); });
+	$(".c46").mousedown(function(e){ $(".c46").css("display","none"); a++; Abol(); });
+	function Abol()
+	{
+		if (once == 0)
+		{
+			setInterval(bahal,500);
+			setInterval(timer,1000);
+			once = 1;
+		}
+		if (a == 46)
+		{
+			alert(" رکورد شما "+second+" ثانیه ");
+			alert("دایره ها نابود شدند");
+			alert("بزن f5 برای بازی مجدد");
+		}
+	}
+	function timer()
+	{
+		second++;
+	}
+	function bahal()
+	{
+
+	}
+});
